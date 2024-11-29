f35b027bc3984f7e4dd32a2a04e1315a6c6a36a4
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2024-11-29 11:45:31 +0100

    Ajout de la gestion de l'état pour le composant CheckboxReponse dans WrapperQuizz

--- src/components/CheckboxResponse.jsx
+++ src/components/CheckboxResponse.jsx
@@ -1,7 +1,13 @@
-export function CheckboxReponse() {
+/* eslint-disable react/prop-types */
+export function CheckboxReponse({ isChecked, onChange }) {
   return (
     <div className="form-control">
-      <input className="checkbox" type="checkbox" />
+      <input
+        className="checkbox"
+        type="checkbox"
+        checked={isChecked}
+        onChange={onChange}
+      />
     </div>
   );
 }
