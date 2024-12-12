47b46b862ca7bf83082e16f5933145a61e5f54e8
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2024-12-12 08:38:54 +0100

    Refactor import statement in Quiz component

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from "react";
+import { useState } from "react";
 
 export const Quiz = ({ quizData }) => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
