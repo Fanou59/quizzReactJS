76c506793553fa62d6aa9c84e2e02ae652257beb
Ste[PH]ane <steph.guery@gmail.com>
2024-12-13 21:08:59 +0100

    Mise à jour de la configuration de Tailwind CSS pour inclure les chemins de contenu

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@
 //     themes: ["cupcake"],
 //   },
 // };
+// tailwind.config.js
 import typography from "@tailwindcss/typography";
 import daisyui from "daisyui";
 
 export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [typography, daisyui],
 };
