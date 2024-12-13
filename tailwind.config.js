3da1e4f2406e7487bccad71efbd363a693caa06c
Ste[PH]ane <steph.guery@gmail.com>
2024-12-13 21:50:49 +0100

    Refactor la configuration de Tailwind CSS pour restaurer les imports et simplifier la syntaxe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,11 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("@tailwindcss/typography"), require("daisyui")],
-//   daisyui: {
-//     themes: ["cupcake"],
-//   },
-// };
-// tailwind.config.js
-import typography from "@tailwindcss/typography";
-import daisyui from "daisyui";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [typography, daisyui],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["cupcake"],
+  },
 };
