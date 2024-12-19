04f765f7226297d7c213d3ce32de4199abba826f
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2024-12-19 16:33:20 +0100

    Refactorisation du composant AnswerProposal pour utiliser des boutons radio à la place des boutons classiques

--- src/components/AnswerProposal.jsx
+++ src/components/AnswerProposal.jsx
@@ -1,22 +1,9 @@
-// Todo refaire le composant en utilisant un bouton radio pour la réponse
-
-// export const AnswerProposal = ({ children, onClick }) => {
-//   return (
-//     <button
-//       className="mt-1 text-gray-500 hover:bg-zinc-200 hover:rounded-xl hover:px-4 active:bg-zinc-300"
-//       onClick={onClick}
-//     >
-//       {children}
-//     </button>
-//   );
-// };
-
 export const AnswerProposal = ({ children, onChange, isSelected }) => {
   return (
     <div className="flex items-center mt-1">
       <input
         type="radio"
-        className="form-radio h-5 w-5 text-zinc-500"
+        className="radio"
         onChange={onChange}
         checked={isSelected}
         readOnly
