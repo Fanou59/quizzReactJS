7174e608cede7bb8aa8757493eca90cf2cfdd470
Ste[PH]ane <steph.guery@gmail.com>
2025-01-02 14:06:00 +0100

    Ajout d'une option par défaut dans le sélecteur de thème pour améliorer l'expérience utilisateur

--- src/components/SelectTheme.jsx
+++ src/components/SelectTheme.jsx
@@ -3,7 +3,11 @@ export const SelectTheme = ({ themes, isSelected }) => {
     <select
       className="select select-info w-full max-w-xs capitalize"
       onChange={isSelected}
+      defaultValue=""
     >
+      <option value="" disabled>
+        -- Sélectionner un thème --
+      </option>
       {themes.map((theme, index) => (
         <option key={index} value={theme.name}>
           {theme.name}
